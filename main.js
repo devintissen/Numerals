f77ebbf7fe74b10e538583d184176eb3052d9d48
FreddyFred12331 <148292971+FreddyFred12331@users.noreply.github.com>
2026-03-29 17:45:18 -0500

    Started working on user interface, resized window to rough dimensions

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
+    width: 290,
+    height: 500,
+    titleBarStyle: 'hidden', trafficLightPosition: { x: 15, y: 15 },
+    resizable: false,
   })
 
   win.loadFile('index.html')
