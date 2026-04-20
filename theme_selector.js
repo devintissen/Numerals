e287fdae0a8198ca093926490e97f800b902efec
FreddyFred12331 <148292971+FreddyFred12331@users.noreply.github.com>
2026-04-19 20:11:05 -0500

    Got the Theme Selector Working

--- theme_selector.js
+++ theme_selector.js
@@ -1,18 +1,25 @@
-const themeOptions = document.querySelectorAll('.theme-option:not(.disabled)');
+const themeOptions = document.querySelectorAll('.Themes_Selector_Button:not([disabled])');
 
-themeOptions.forEach(option => {
-    option.addEventListener('click', () => {
-    const currentSelected = document.querySelector('.theme-option.selected');
+function selectThemeButton(button) {
+    const currentSelected = document.querySelector('.Themes_Selector_Button.selected');
     if (currentSelected) {
         currentSelected.classList.remove('selected');
+        currentSelected.setAttribute('aria-pressed', 'false');
     }
 
-    option.classList.add('selected');
+    button.classList.add('selected');
+    button.setAttribute('aria-pressed', 'true');
 
-    const selectedTheme = option.dataset.theme;
+    const selectedTheme = button.dataset.theme;
+    document.body.setAttribute('data-theme', selectedTheme);
     console.log('Selected theme:', selectedTheme);
+}
 
-    // Example for later:
-    // document.body.setAttribute('data-theme', selectedTheme);
-    });
+themeOptions.forEach(option => {
+    option.setAttribute('aria-pressed', 'false');
+    option.addEventListener('click', () => selectThemeButton(option));
 });
+
+if (themeOptions.length > 0) {
+    selectThemeButton(themeOptions[0]);
+}
